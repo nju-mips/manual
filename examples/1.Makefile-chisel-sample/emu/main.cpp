c58662d6d67070763e1e902d5e0b8c90fabd1b8f
ouxianfei <ouxianfei@smail.nju.edu.cn>
2022-03-06 11:05:15 +0800

    sync build.sbt and Makefiles

--- examples/1.Makefile-chisel-sample/emu/main.cpp
+++ examples/1.Makefile-chisel-sample/emu/main.cpp
@@ -1,24 +1,35 @@
 #include "emu.h"
-#include <memory>
 #include <iostream>
+#include <memory>
+#include <stdint.h>
 #include <verilated.h>
 
+extern "C" {
+void klee_make_symbolic(void *, int, const char *);
+void klee_assume(int);
+}
+
 int main(int argc, char **argv) {
-  auto dut = std::make_shared<emu>();
-  dut->reset = 0;
-  for (int i = 0; i < 10; i++) {
-    dut->io_in_valid = 1;
-    dut->io_in_bits_op = i;
-    dut->io_in_bits_a = 123;
-    dut->io_in_bits_b = 456;
+  emu dut{};
+  dut.reset = 0;
 
-    dut->clock = 0;
-    dut->eval();
+  uint32_t symbols[3];
+  klee_make_symbolic(symbols, sizeof(symbols), "input");
 
-    dut->clock = 1;
-    dut->eval();
+  dut.io_in_valid = 1;
+  dut.io_in_bits_op = symbols[0];
+  dut.io_in_bits_a = symbols[1];
+  dut.io_in_bits_b = symbols[2];
+  klee_assume(symbols[0] < 8);
 
-	printf ("RECEIVE: %d\n", dut->io_out_bits_c);
+  while (true) {
+    dut.clock = 0;
+    dut.eval();
+    dut.clock = 1;
+    dut.eval();
+
+    if (dut.io_out_bits_c == 0x12345678) break;
   }
+  assert(0);
   return 0;
 }
